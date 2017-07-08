d97bd4c299add5382b0da901f26ae43dee2cd7f2
eric1234463 <eric1234463@gmail.com>
2017-07-08 20:24:36 +0800

    finish ui

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -12,11 +12,13 @@ import { TabsPage } from '../pages/tabs/tabs';
 import { SignInPage } from '../pages/signin/signin';
 import { SignUpPage } from '../pages/signup/signup';
 import { DetailPage } from '../pages/home/detail/detail';
+import { CreatePage } from '../pages/home/create/create';
 
 import { StatusBar } from '@ionic-native/status-bar';
 import { SplashScreen } from '@ionic-native/splash-screen';
 import { AngularFireDatabaseModule } from 'angularfire2/database';
 import { AngularFireAuthModule } from 'angularfire2/auth';
+import { Ionic2RatingModule } from 'ionic2-rating';
 
 export const firebaseConfig = {
     apiKey: "AIzaSyBSbQ9pC0t76hvQPNt1o4kDgdlyAsia8Vk",
@@ -36,14 +38,16 @@ export const firebaseConfig = {
     TabsPage,
     SignInPage,
     SignUpPage,
-    DetailPage
+    DetailPage,
+    CreatePage
   ],
   imports: [
     BrowserModule,
     IonicModule.forRoot(MyApp),
     AngularFireModule.initializeApp(firebaseConfig),
     AngularFireDatabaseModule,
-    AngularFireAuthModule
+    AngularFireAuthModule,
+    Ionic2RatingModule
   ],
   bootstrap: [IonicApp],
   entryComponents: [
@@ -54,7 +58,8 @@ export const firebaseConfig = {
     TabsPage,
     SignInPage,
     SignUpPage,
-    DetailPage
+    DetailPage,
+    CreatePage
   ],
   providers: [
     StatusBar,
