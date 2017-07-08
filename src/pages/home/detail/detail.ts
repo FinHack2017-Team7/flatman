1343645545f993aad0cd5e62bfa59c38e29bb405
eric1234463 <eric1234463@gmail.com>
2017-07-08 22:10:14 +0800

    add Finanical

--- src/pages/home/detail/detail.ts
+++ src/pages/home/detail/detail.ts
@@ -1,16 +1,24 @@
 import { Component } from '@angular/core';
-import { NavController , NavParams} from 'ionic-angular';
+import { NavController , NavParams,App} from 'ionic-angular';
 import { AngularFireDatabase, FirebaseObjectObservable } from 'angularfire2/database';
+import { TabsPage } from '../../tabs/tabs';
+
+
 @Component({
   selector: 'page-detail',
   templateUrl: 'detail.html'
 })
 export class DetailPage {
   public flat: FirebaseObjectObservable<any>;
-  constructor(public navCtrl: NavController,public db: AngularFireDatabase,public navParams: NavParams) {
+  constructor(public navCtrl: NavController,public db: AngularFireDatabase,public navParams: NavParams , public appCtrl : App) {
       this.navCtrl = navCtrl;
       this.navParams = navParams;
       this.flat = this.navParams.get('flat');
+  }
+
+  joinUs(){
+    this.db.list('/favourite').push(this.flat);
+    this.navCtrl.setRoot(TabsPage);
   }
 
 
