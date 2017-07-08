fa5ca5da8def922a83ddf294efc68588f1f12ae4
Zander Olidan <kaikaiolidan@gmail.com>
2017-07-09 01:03:21 +0800

    profile details

--- src/pages/profile/profile.ts
+++ src/pages/profile/profile.ts
@@ -11,6 +11,18 @@ import { SettingsPage } from './settings/settings';
 
 export class ProfilePage {
   public user : any;
+  public infos: any[] = [
+    {
+      field: "Phone Number", 
+      icon: "call",
+      value: "52413392"
+    },
+    {
+      field: "Gender",
+      icon: "contact",
+      value: "Male"
+    }
+   ];
 
   constructor(public navCtrl: NavController, public appCtrl: App, public afAuth: AngularFireAuth) {
     this.afAuth.authState.subscribe(user => {
