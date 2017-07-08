11c82ce341c228b78044f88df4140cf0f41898dd
Zander Olidan <kaikaiolidan@gmail.com>
2017-07-09 02:36:23 +0800

    Favourites and Home merged into Home tab

--- src/pages/tabs/tabs.ts
+++ src/pages/tabs/tabs.ts
@@ -1,7 +1,6 @@
 import { Component } from '@angular/core';
 
 import { HomePage } from '../home/home';
-import { FavouritesPage } from '../favourites/favourites';
 import { ProfilePage } from '../profile/profile';
 import { BankPage } from '../bank/bank';
 import { NotificationPage } from '../notification/notification';
@@ -13,7 +12,6 @@ export class TabsPage {
 
   tab1Root = HomePage;
   tab2Root = BankPage;
-  tab3Root = FavouritesPage;
   tab4Root = NotificationPage;
   tab5Root = ProfilePage;
 
